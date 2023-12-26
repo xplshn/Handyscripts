345c96c5e6a6ad50e23da6a2cc6cb6104e9d3283
xplshn <114888778+xplshn@users.noreply.github.com>
2023-12-26 12:40:24 +0000

    Add files via upload

--- .std.h.sh
+++ .std.h.sh
@@ -4,17 +4,32 @@
 # spinner() require() palette255
 
 # Display a spinner loader with the colors declared(or not!) in the COOLSPINNERCOLOR variable
+# Display a spinner loader with default colors
 spinner() {
-    [ -z "$COOLSPINNER" ] && COOLSPINNER='|/~\\'
-    [ -z "$COOLSPINNER_COLOR" ] && COOLSPINNERCOLOR='\033[0m'
-    [ -z "$COOLSPINNER_DELAY"] && COOLSPINNER_DELAY=0.1
-    len=${#COOLSPINNER}
+    if [ -z "$COOLSPINNER" ]; then
+        COOLSPINNER='|/~\\'
+    fi
+
+    if [ -z "$COOLSPINNER_COLOR" ]; then
+        COOLSPINNER_COLOR='\033[0m'
+    fi
+
+    if [ -z "$COOLSPINNER_DELAY" ]; then
+        COOLSPINNER_DELAY=0.1
+    fi
+
+    len=$(printf "%s" "$COOLSPINNER" | wc -c | awk '{print $1}')
     trap 'printf "\033[?25h"; exit' INT
+
     while true; do
-        i=0
-        while [ "$i" -lt "$len" ]; do
-            char=$(printf "%s" "$COOLSPINNER" | cut -c "$((i + 1))")
-            [ -n "$COOLSPINNERCOLOR" ] && printf "%b%s%b" "$COOLSPINNERCOLOR" "$char" "\033[0m" || printf "%s" "$char"
+        i=1
+        while [ "$i" -le "$len" ]; do
+            char=$(printf "%s" "$COOLSPINNER" | cut -c "$i")
+            if [ -n "$COOLSPINNER_COLOR" ]; then
+                printf "%b%s%b" "$COOLSPINNER_COLOR" "$char" "\033[0m"
+            else
+                printf "%s" "$char"
+            fi
             sleep "$COOLSPINNER_DELAY"
             printf "\b"
             i=$((i + 1))
